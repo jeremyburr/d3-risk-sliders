5462302c93d313579fb0cf32ae31aa63f37e2811
jeremyburr <jeremy.burr73@gmail.com>
2018-08-31 12:10:59 -0400

    replaced tabs with spaces

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import "./styles.css"
 
 class App extends Component {
 
-	renderScale = (category,value) => {
+  renderScale = (category,value) => {
 
-		let string = category;
-		let id = string.replace(/\s+/g,""); 
-			return (
-				<Scale id={id} category={category} value={value} />
-			) 
-	}
+    let string = category;
+    let id = string.replace(/\s+/g,""); 
+      return (
+        <Scale id={id} category={category} value={value} />
+      ) 
+  }
   render() {
     return (
       <div className="callout" >
-				{this.renderScale("Fire Risk",1)}
-				{this.renderScale("Flood Risk",5)}
-				{this.renderScale("Hurricane Risk",8)}
-				{this.renderScale("Storm Risk",9)}
-				{this.renderScale("Crime Rate",2)}
-			</div>
+        {this.renderScale("Fire Risk",1)}
+        {this.renderScale("Flood Risk",5)}
+        {this.renderScale("Hurricane Risk",8)}
+        {this.renderScale("Storm Risk",9)}
+        {this.renderScale("Crime Rate",2)}
+      </div>
     );
   }
 }
